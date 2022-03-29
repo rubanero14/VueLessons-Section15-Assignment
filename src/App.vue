854255134e96da81ba3d31df2f36683f94dc1197
Rajaruban Rajindram <ahyat888@gmail.com>
2022-03-29 13:12:39 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <the-header></the-header>
+  <router-view></router-view>
+</template>
+
+<script>
+import TheHeader from './components/nav/TheHeader.vue';
+
+export default {
+  components: {
+    TheHeader
+  },
+  provide() {
+    return {
+      isLoggedIn: this.isLoggedIn,
+      cart: this.cart,
+      addProductToCart: this.addProductToCart,
+      removeProductFromCart: this.removeProductFromCart,
+      login: this.login,
+      logout: this.logout,
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
